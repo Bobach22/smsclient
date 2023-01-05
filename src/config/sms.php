9fee3f363da16c474ef26fa3a3f5ceff0b11509d
makhmudovazeez <azeezmakhmudov@gmail.com>
2023-01-05 17:24:31 +0500

    config file

--- src/config/sms.php
+++ src/config/sms.php
@@ -2,7 +2,6 @@
 
 return [
     'headers'=>[
-        'Authorization'=>'Bearer '.env('SMS_TOKEN'),
         'Content-Type'=>'application/json'
     ],
     'params'=>[
@@ -10,9 +9,12 @@ return [
         'number_key'             => env('SMS_NUMBER_KEY'),
         'message_key'            => env('SMS_MESSAGE_KEY'),
         'service_url'            => env('SMS_SERVICE_URL'),
+        'eskiz_auth'             => env('ESKIZ_AUTH'),
         'service_bulk_send_url'  => env('SMS_SERVICE_BULK_SEND_URL'),
         'method'                 => env('SMS_METHOD'),
-        'from'                   => env('SMS_FROM')
+        'from'                   => env('SMS_FROM'),
+        'email'                  => env('EMAIL'),
+        'password'               => env('PASSWORD'),
     ],
     'cache_expired_time' => env('SMS_TOKEN_EXPIRED_IN')
 ];
